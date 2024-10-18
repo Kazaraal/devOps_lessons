099997631ec1dbf0e99b0ea3bbeeb98c2331cfc0
kazaraal <araalkaz@gmail.com>
2024-10-18 05:42:28 +0100

    This is lesson 7

---
+++ lesson_7.py
@@ -0,0 +1,26 @@
+# Started by user ken
+# Running as SYSTEM
+# Building in workspace /var/lib/jenkins/workspace/lesson 7
+# The recommended git tool is: NONE
+# No credentials specified
+#  > git rev-parse --resolve-git-dir /var/lib/jenkins/workspace/lesson 7/.git # timeout=10
+# Fetching changes from the remote Git repository
+#  > git config remote.origin.url https://github.com/Kazaraal/MySoftware.git # timeout=10
+# Fetching upstream changes from https://github.com/Kazaraal/MySoftware.git
+#  > git --version # timeout=10
+#  > git --version # 'git version 2.43.0'
+#  > git fetch --tags --force --progress -- https://github.com/Kazaraal/MySoftware.git +refs/heads/*:refs/remotes/origin/* # timeout=10
+#  > git rev-parse refs/remotes/origin/master^{commit} # timeout=10
+# Checking out Revision cb0f12f8b2c1b1838102b8a755148055af0bb978 (refs/remotes/origin/master)
+#  > git config core.sparsecheckout # timeout=10
+#  > git checkout -f cb0f12f8b2c1b1838102b8a755148055af0bb978 # timeout=10
+# Commit message: "Merge pull request #2 from Kazaraal/NewScreen"
+#  > git rev-list --no-walk cb0f12f8b2c1b1838102b8a755148055af0bb978 # timeout=10
+# [lesson 7] $ /bin/sh -xe /tmp/jenkins5748536422697397924.sh
+# + python3 click_method.py
+# click
+# + python3 welcome_method.py
+# New Screen
+# Finished: SUCCESS
+
+
